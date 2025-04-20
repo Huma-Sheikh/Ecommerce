48622ad45944a877514d97883c49095a2a55928b
Huma-Sheikh <humashafique24@gmail.com>
2025-04-20 19:45:24 +0500

    added two new pages as well as modified four components

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 
-const Footer = () => {
+const About = () => {
   return (
     <footer>
       <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500">
@@ -41,16 +41,14 @@ const Footer = () => {
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
               <p>+1-234-567-890</p>
-              <p>contact@greatstack.dev</p>
+              <p>contact@ecommerce-projects</p>
             </div>
           </div>
         </div>
       </div>
-      <p className="py-4 text-center text-xs md:text-sm">
-        Copyright © 2025 .All Right Reserved.
-      </p>
+     
     </footer>
   );
 };
 
-export default Footer;
+export default About;
